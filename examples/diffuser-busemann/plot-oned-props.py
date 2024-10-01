41ed178cd0cd3ad80446d2d6c0a82c8e8f4fa3e4
Rowan J. Gollan <r.gollan@uq.edu.au>
2024-10-01 17:59:06 +1000

    pingvin: Update plotting of one-d properties based on new averaging.

--- examples/diffuser-busemann/plot-oned-props.py
+++ examples/diffuser-busemann/plot-oned-props.py
@@ -7,10 +7,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-FNAME = "pingvin-oned-stream-thrust-avg.data"
+FNAME = "pingvin-oned-cmes-avg.data"
 
 # Only interested in a subset of data, so pick that out by column
-x, area, p, Mach = np.loadtxt(FNAME, skiprows=1, usecols=(0, 1, 2, 5), unpack=True)
+x, area, p, Mach, pt = np.loadtxt(FNAME, skiprows=1, usecols=(0, 1, 2, 5, 7), unpack=True)
 
 fig, ax = plt.subplots()
 fig.subplots_adjust(right=0.75)
@@ -20,21 +20,22 @@ twin2.spines['right'].set_position(("axes", 1.125))
 
 p0, = ax.plot(x, p/1000.0, label='pressure, kPa', color='red')
 p1, = twin1.plot(x, Mach, label='Mach number', color='blue')
-p2, = twin2.plot(x, area, label='area, m^2', color='black')
+p2, = ax.plot(x, area/area[-1], label='contraction ratio', color='black')
+p3, = twin2.plot(x, pt/pt[0], label='total pressure recovery', color='green')
 
 twin1.yaxis.label.set_color(p1.get_color())
-twin2.yaxis.label.set_color(p2.get_color())
+twin2.yaxis.label.set_color(p3.get_color())
 
-ax.legend(handles=[p0, p1, p2], loc='center left', frameon=True, framealpha=1, edgecolor="black")
-ax.set_ylabel("pressure, kPa")
+ax.legend(handles=[p0, p1, p2, p3], loc=(0.05,0.7), frameon=True, framealpha=1, edgecolor="black", prop={'size':6})
+ax.set_ylabel("pressure, kPa; contraction ratio")
 ax.set_xlabel("x, m")
 twin1.set_ylabel("Mach number")
-twin2.set_ylabel("area, m^2")
+twin2.set_ylabel("total pressure recovery")
 twin1.set_ylim(0, 6)
-twin2.set_ylim(0, 1.0)
+twin2.set_ylim(0, 1.1)
 tkw = dict(size=4, width=1.5)
 twin1.tick_params(axis='y', colors=p1.get_color(), **tkw)
-twin2.tick_params(axis='y', colors=p2.get_color(), **tkw)
+twin2.tick_params(axis='y', colors=p3.get_color(), **tkw)
 
 
 plt.savefig("busemann-oned-props.png", dpi=600)
