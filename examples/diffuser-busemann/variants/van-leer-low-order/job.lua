4ea9e97848bffdc2b0388fcf99122d6e7f8685b9
Rowan J. Gollan <r.gollan@uq.edu.au>
2025-02-01 20:20:22 +1000

    pingvin: Add example job.lua for calling van Leer fluxes

---
+++ examples/diffuser-busemann/variants/van-leer-low-order/job.lua
@@ -0,0 +1,19 @@
+-- Pingvin input file for: buseman-diffuser
+grid2d_file = "qcirc.su2"
+cross_section_dir = "xsect"
+no_cross_sections = 100
+dx = 6.44/1000
+
+-- inflow conditions
+Mach_inflow = 5.76788
+p_inflow = 1000.0 -- Pa
+T_inflow = 250.0  -- K
+
+flux_calculator = "van-leer"
+streamwise_flux_reconstruction = false
+
+-- solver settings
+max_newton_steps = 10
+slice_relative_residual = 1.0e-9
+max_gmres_iterations = 50
+gmres_relative_residual = 1.0e-4
